45a59aa663145d9c73d015bd821cb3051dd81959
Mrm <86636997+noc-turne@users.noreply.github.com>
2024-08-12 01:06:35 +0800

    Time: 9 ms (12.53%), Space: 16.9 MB (15.88%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-
         int l = 0;
         int r = nums.size() - 1;
-        int cnt = 0;
+        bool up = false, down = false;
+        if(target == nums[l]) return true;
+        if(target > nums[l]) up = true;
+        else down = true;
         while(l < r) {
             int mid = (l + r) / 2;
-            if(nums[mid] == target) return true;
-            if(target == nums[l]) return true;
-            else if(target > nums[l]) {
-                if(nums[mid] > target) {
-                    r = mid - 1;
-                }
+            if(nums[mid] == nums[0]) {
+                if(nums[l] == target) return true;
+                l ++;
+            }
+            else if(nums[mid] > nums[0]) {
+                if(nums[mid] == target) return true;
+                else if(nums[mid] > target) {
+                    if(up) r = mid - 1;
+                    if(down) l = mid + 1;
+                } 
                 else {
-                    if(nums[mid] < nums[l]) {
-                        r = mid;
-                    }
-                    else if(nums[mid] > nums[l]) {
-                        l = mid;
-                    }
-                    else {
-                        l ++;
-                    }
+                    l = mid + 1;
                 }
             }
             else {
-                if(nums[mid] <= target) {
-                    l = mid;
+                if(nums[mid] == target) return true;
+                else if(nums[mid] > target) {
+                    r = mid - 1;
                 }
                 else {
-                    if(nums[mid] > nums[l]) {
-                        l = mid + 1;
-                    }
-                    else if(nums[mid] < nums[l]) {
-                        r = mid - 1;
-                    }
-                    else {
-                        l ++;
-                    }
+                    if(up) r = mid - 1;
+                    if(down) l = mid + 1;
                 }
-
             }
+            cout << l << ' ' << r << endl;
         }
-        if(l < nums.size() && nums[l] == target) return true;
+
+        if(nums[l] == target) return true;
         return false;
         
     }
