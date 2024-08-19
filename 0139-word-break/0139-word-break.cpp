fead7633fc41598d3be415eef6f73094de932c7d
Mrm <86636997+noc-turne@users.noreply.github.com>
2024-08-19 14:44:31 +0800

    Time: 2 ms (88.89%), Space: 10.3 MB (88.7%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<bool> dp(n+1, false);
+        dp[0] = true;
+        for(int i=1;i<=n;i++) {
+            for(const string &k:wordDict) {
+                int len = k.size();
+                if(i >= len && s.substr(i-len,len) == k) {
+                    dp[i] = dp[i-len];
+                }
+                if(dp[i]) break;
+            }
+        }
+
+        return dp[n];
+
+        
+    }
+};
